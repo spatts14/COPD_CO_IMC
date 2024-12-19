5cfa853bffebc512a6f25a8034c7fb071d91c1bf
Sara Patti <sep22@ic.ac.uk>
2024-12-19 17:48:34 +0000

    Adding preprocessing scripts, work in progress

---
+++ scripts/02_cell_segmentation.sh
@@ -0,0 +1,24 @@
+# $ Set up Steinbock alias
+# 1. Set up Stienbock command alias
+alias steinbock="docker run -v /Users/sarapatti/Desktop/PhD_projects/Llyod_lab/COPD_CO_IMC/data:/data --platform linux/amd64 -u $(id -u):$(id -g) -p 8888:8888 -v /tmp/.X11-unix:/tmp/.X11-unix -v ~/.Xauthority:/home/steinbock/.Xauthority:ro -e DISPLAY=host.docker.internal:0 ghcr.io/bodenmillergroup/steinbock:0.16.1"
+
+
+# ! OPTION: Can either preform cell segmentation with DeepCell or Illastik + CellProfiler 
+
+# ! Option 1: DeepCell
+# # * DeepCell
+# # 2. Deep cell - cell segmentation
+# steinbock segment deepcell --app mesmer --minmax
+
+
+# ! Option 2: Ilastik and CellProfiler
+# TODO: train ilastik classifier
+# $ Pixel classification using ilastik
+# 3. Prepare the data for ilastik
+steinbock classify ilastik prepare --cropsize 50 --seed 123
+# 4. Train classifier - To interactively train a new classifier, open the pixel classification project in Ilastik
+steinbock apps ilastik
+# 5. Batch processing - applied to a batch of full-size images
+steinbock classify ilastik run
+# this will create probability images in ilastik_probabilities, with one color per class encoding the probability of pixels belonging to that class 
+# red = nuclei, green = cytoplasm, blue = background
