c6620a04c888f50d636d1071d34bb7cfd4fd4e8a
Sara Patti <sep22@ic.ac.uk>
2024-12-19 23:11:53 +0000

    cell segmentation script updated, still need to update cell profiler

--- scripts/02_cell_segmentation.sh
+++ scripts/02_cell_segmentation.sh
@@ -5,16 +5,26 @@ alias steinbock="docker run -v /Users/sarapatti/Desktop/PhD_projects/Llyod_lab/C
 
 # ! OPTION: Can either preform cell segmentation with DeepCell or Illastik + CellProfiler 
 
-# ! Option 1: DeepCell
+# $ Option 1: DeepCell
 # # * DeepCell
 # # 2. Deep cell - cell segmentation
 steinbock segment deepcell --app mesmer --minmax
 
 
-# ! Option 2: Ilastik and CellProfiler
-# TODO: train ilastik classifier
-# $ Pixel classification using ilastik
+# $ Option 2: Ilastik and CellProfiler
+# * Pixel classification using ilastik
 # 3. Prepare the data for ilastik
 steinbock classify ilastik prepare --cropsize 50 --seed 123
 # 4. Train classifier - To interactively train a new classifier, open the pixel classification project in Ilastik
 steinbock apps ilastik
+
+# TODO: determmine how to use CellProfiler
+# * CellProfiler
+# 5. Pipeline preperation - CellProfiler pipeline is prepared for processing the images
+steinbock segment cellprofiler prepare
+
+# 6. Run the pipeline - CellProfiler pipeline is run on the images
+steinbock apps cellprofiler
+
+# 7. Batch processing: After the pipeline has been configured, it can be applied to a batch of probability images
+steinbock segment cellprofiler run
