bf72dcff6f89b23a3909eb37df1d73fa41c6a6f5
Sara Patti <sep22@ic.ac.uk>
2025-01-21 15:34:30 +0000

    removed old files that did not have variables in config

--- scripts/00_napari_imc.sh
+++
@@ -1,9 +0,0 @@
-# 1. Create a new conda environment
-# 2. Install napari-imc
-# 3. Activate the conda environment
-conda activate napari-imc
-
-# 4. Run napari-imc
-napari
-
-# 5. Drag and drop txt files into the napari window
